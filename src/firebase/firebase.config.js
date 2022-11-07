e4e8c8a778777373aec5f4f0c4802a9894396e21
Miraz9907 <meraz.rahman99@gmail.com>
2022-11-07 22:55:54 +0600

    project setup with tailwind

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAG7OG7_5oRMjl9mKecdkH8wzFVK4fic4U",
+  authDomain: "health-coach-83680.firebaseapp.com",
+  projectId: "health-coach-83680",
+  storageBucket: "health-coach-83680.appspot.com",
+  messagingSenderId: "26118276569",
+  appId: "1:26118276569:web:39755c9149c0afea7e45c8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
