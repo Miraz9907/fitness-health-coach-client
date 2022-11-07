4bdbe6840e3928ca836cd1c1f2add4d72c3d35f4
Miraz9907 <meraz.rahman99@gmail.com>
2022-11-08 02:10:17 +0600

    logout suthentication

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -14,6 +14,7 @@ const Login = () => {
         .then(result => {
             const user = result.user;
             console.log(user);
+            form.reset();
 
         })
         .catch(err => console.error(err))
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -6,7 +6,9 @@ const Header = () => {
     const {user,logOut} = useContext(AuthContext);
 
     const handleLogOut =() =>{
-        
+        logOut()
+        .then( ()=>{})
+        .catch(()=>{})
     }
     const menuItems = <>
     <li className='font-semibold'><Link to='/'>Home</Link></li>
@@ -32,9 +34,9 @@ const Header = () => {
         <div className="navbar h-20 mb-12 pt-12 bg-base-100">
   <div className="navbar-start">
     <div className="dropdown">
-      {/* <label tabIndex={0} className="btn btn-ghost lg:hidden">
+      <label tabIndex={0} className="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-      </label> */}
+      </label>
       <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
         {menuItems}
       </ul>
@@ -50,7 +52,7 @@ const Header = () => {
     </ul>
   </div>
   <div className="navbar-end">
-  <button className="btn btn-outline btn-warning">Appointment</button>
+  <button className="btn btn-active btn-secondary">mail me</button>
 
   </div>
 </div>
